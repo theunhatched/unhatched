9b0f14b646a4911f753017544924bf9f1e1f75ea
Philihp Busby <philihp@gmail.com>
2020-03-30 15:59:27 +0100

    remove cloudformation stuff

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,7 +3,6 @@ provider "aws" {
   region  = "${var.region}"
 }
 
-
 locals {
   aws_vpc_stack_name          = "${var.aws_resource_prefix}-vpc-stack"
   aws_ecs_service_stack_name  = "${var.aws_resource_prefix}-svc-stack"
@@ -17,27 +16,12 @@ resource "aws_ecr_repository" "blastocyst" {
   name = "${local.aws_ecr_repository_name}"
 }
 
-resource "aws_cloudformation_stack" "vpc" {
-  name          = "${local.aws_vpc_stack_name}"
-  template_body = file("templates/public-vpc.yml")
-  capabilities  = ["CAPABILITY_NAMED_IAM"]
-  parameters = {
-    ClusterName       = "${local.aws_ecs_cluster_name}"
-    ExecutionRoleName = "${local.aws_ecs_execution_role_name}"
-  }
+resource "aws_elastic_beanstalk_application" "amnion" {
+  name = "amnion"
 }
 
-resource "aws_cloudformation_stack" "ecs_service" {
-  name          = "${local.aws_ecs_service_stack_name}"
-  template_body = file("templates/public-service.yml")
-  depends_on    = [aws_cloudformation_stack.vpc, aws_ecr_repository.blastocyst]
-
-  parameters = {
-    ContainerMemory = 1024
-    ContainerPort   = 80
-    StackName       = "${local.aws_vpc_stack_name}"
-    ServiceName     = "${local.aws_ecs_service_name}"
-    # Note: Since ImageUrl parameter is not specified, the Service
-    # will be deployed with the nginx image when created
-  }
+resource "aws_elastic_beanstalk_environment" "amnion-env" {
+  name                = "amnion-env"
+  application         = "${aws_elastic_beanstalk_application.tftest.name}"
+  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
 }
