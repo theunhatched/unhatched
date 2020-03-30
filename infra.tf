8b8bd97e15a91d7c53609028d9b097f55dad5b1e
Philihp Busby <philihp@gmail.com>
2020-03-30 10:41:37 +0100

    dont add these via docker

--- infra.tf
+++ infra.tf
@@ -1,9 +1,32 @@
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0fc61db8544a617ed"
+resource "aws_key_pair" "id_rsa" {
+  key_name = "id_rsa"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "primary" {
+  key_name = aws_key_pair.id_rsa.key_name
+  ami = "ami-0fc61db8544a617ed"
   instance_type = "t3.nano"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.primary.id
+}
+
+module "consul" {
+  source      = "hashicorp/consul/aws"
+  num_servers = "3"
 }
