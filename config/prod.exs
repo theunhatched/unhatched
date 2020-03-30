1fbc5f3b50deccaa98e52e20bdd8b69cefd8c5ec
Philihp Busby <philihp@gmail.com>
2020-03-30 11:07:22 +0100

    container default port 4000

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :unhatched_web, UnhatchedWeb.Endpoint,
-  url: [host: System.get_env("APP_HOSTNAME") || "localhost", port: 80],
+  url: [
+    host: System.get_env("APP_HOSTNAME") || "localhost",
+    port: System.get_env("PORT") || 4000
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
