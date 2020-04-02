21ceca0b1f3a379f1e53940e141d9c9fb120fd61
Philihp Busby <philihp@gmail.com>
2020-04-02 17:28:11 +0000

    buildspec updates

--- terraform/amnion.tf
+++ terraform/amnion.tf
@@ -44,6 +44,19 @@ resource "aws_codebuild_project" "amnion" {
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
+
+    # environment_variable {
+    #   name  = "AWS_DEFAULT_REGION"
+    #   value = "us-east-1"
+    # }
+    environment_variable {
+      name  = "IMAGE_REPO_NAME"
+      value = "${local.aws_ecr_repository_name}"
+    }
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = "${data.aws_caller_identity.current.account_id}"
+    }
   }
 
   logs_config {
