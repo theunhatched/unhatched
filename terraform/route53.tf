b2e91ae4e88f9933df914dfd32a22f20ebf0a771
Philihp Busby <philihp@gmail.com>
2020-04-02 05:02:48 +0000

    dns setup:

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,10 +3,18 @@ resource "aws_route53_zone" "selected" {
   name = "theunhatched.com"
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "selected" {
   zone_id = "${aws_route53_zone.selected.zone_id}"
   name    = "${aws_route53_zone.selected.name}"
   type    = "A"
-  ttl     = 30
-  records = ["10.0.0.1"]
+  alias {
+    name                   = "${module.alb.dns_name}"
+    zone_id                = "${module.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+output "nameservers" {
+  description = "Nameservers"
+  value       = "${aws_route53_zone.selected.name_servers}"
 }
