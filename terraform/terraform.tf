f059f7234d70ead6f88b6230f111d874720ef678
Philihp Busby <philihp@gmail.com>
2020-04-02 04:12:51 +0000

    working with full stack

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,7 @@
 locals {
+  aws_name                    = "${var.aws_resource_prefix}"
   aws_ecr_repository_name     = "${var.aws_resource_prefix}"
+  aws_alb_name                = "${var.aws_resource_prefix}-alb"
   aws_vpc_stack_name          = "${var.aws_resource_prefix}-vpc-stack"
   aws_ecs_cluster_name        = "${var.aws_resource_prefix}-cluster"
   aws_ecs_task_name           = "${var.aws_resource_prefix}-task"
@@ -18,60 +20,87 @@ resource "aws_ecr_repository" "blastocyst" {
   name = "${local.aws_ecr_repository_name}"
 }
 
-module "networking" {
-  source                                      = "cn-terraform/networking/aws"
-  name_preffix                                = "base"
-  profile                                     = "default"
-  region                                      = "us-east-1"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+#####
+# VPC and subnets
+#####
+module "vpc" {
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "~> 2.21"
+  name               = "${local.aws_vpc_stack_name}"
+  cidr               = "10.0.0.0/16"
+  azs                = ["us-east-1a", "us-east-1b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = false
 }
 
-module "ecs-fargate" {
-  source                       = "cn-terraform/ecs-fargate/aws"
-  name_preffix                 = "blastocyst"
-  profile                      = "default"
-  region                       = "us-east-1"
-  vpc_id                       = "${module.networking.vpc_id}"
-  availability_zones           = "${module.networking.availability_zones}"
-  public_subnets_ids           = "${module.networking.public_subnets_ids}"
-  private_subnets_ids          = "${module.networking.private_subnets_ids}"
-  container_name               = "blastocyst"
-  container_image              = "blastocyst:latest"
-  container_cpu                = 1024
-  container_memory             = 8192
-  container_memory_reservation = 2048
-  essential                    = true
-  container_port               = 9000
-  # environment = [
-  #     {
-  #         name  = <VARIABLE_NAME>
-  #         value = <VARIABLE_VALUE>
-  #     }
-  # ]
+# #####
+# # ECS cluster and fargate
+# #####
+resource "aws_ecs_cluster" "cluster" {
+  name = "${local.aws_ecs_cluster_name}"
+}
+
+module "ecs-farage" {
+  source                          = "umotif-public/ecs-fargate/aws"
+  name_prefix                     = "${local.aws_name}"
+  vpc_id                          = "${module.vpc.vpc_id}"
+  private_subnet_ids              = "${module.vpc.public_subnets}"
+  lb_arn                          = "${module.alb.arn}"
+  cluster_id                      = "${aws_ecs_cluster.cluster.id}"
+  task_container_image            = "marcincuber/2048-game:latest"
+  task_definition_cpu             = 256
+  task_definition_memory          = 512
+  task_container_port             = 80
+  task_container_assign_public_ip = true
+  health_check = {
+    port = "traffic-port"
+    path = "/"
+  }
 }
 
 
-# resource "aws_ecs_cluster" "blastocyst" {
-#   name = "${local.aws_ecs_cluster_name}"
-# }
+# #####
+# # ALB
+# #####
+module "alb" {
+  source             = "umotif-public/alb/aws"
+  version            = "~> 1.0"
+  name_prefix        = "${local.aws_alb_name}"
+  load_balancer_type = "application"
+  internal           = false
+  vpc_id             = "${module.vpc.vpc_id}"
+  subnets            = "${module.vpc.public_subnets}"
+}
 
-# resource "aws_ecs_task_definition" "blastocyst" {
-#   family                = "${local.aws_ecs_task_name}"
-#   container_definitions = "${file("task-definitions/service.json")}"
+resource "aws_lb_listener" "alb_80" {
+  load_balancer_arn = "${module.alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${module.ecs-farage.target_group_arn}"
+  }
+}
 
-#   volume {
-#     name      = "service-storage"
-#     host_path = "/ecs/service-storage"
-#   }
-# }
+# #####
+# # Security Group Config
+# #####
+resource "aws_security_group_rule" "alb_ingress_80" {
+  security_group_id = "${module.alb.security_group_id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
 
-# resource "aws_ecs_service" "blastocyst" {
-#   name                = "${local.aws_ecs_service_name}"
-#   cluster             = "${aws_ecs_cluster.blastocyst.id}"
-#   task_definition     = "${aws_ecs_task_definition.blastocyst.arn}"
-#   desired_count       = 2
-#   scheduling_strategy = "DAEMON"
-# }
+resource "aws_security_group_rule" "task_ingress_80" {
+  security_group_id        = "${module.ecs-farage.service_sg_id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  source_security_group_id = "${module.alb.security_group_id}"
+}
