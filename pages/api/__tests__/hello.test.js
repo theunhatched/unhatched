7a9571a3296eadca143a7e229376fd063621bcf3
Philihp Busby <philihp@gmail.com>
2020-06-18 23:37:00 +0000

    repair tests

--- pages/api/__tests__/hello.test.js
+++ pages/api/__tests__/hello.test.js
@@ -1,12 +1,14 @@
-import React from 'react'
 import hello from '../hello'
 
-it('runs a test', () => {
-  const req = {}
-  const res = {
-    statusCode: 0,
-    json: jest.fn(),
-  }
-  hello(req, res)
-  expect(res.statusCode).toBe(200)
+describe('hello', () => {
+  it('runs a test', () => {
+    expect.assertions(1)
+    const req = {}
+    const res = {
+      statusCode: 0,
+      json: jest.fn(),
+    }
+    hello(req, res)
+    expect(res.statusCode).toBe(200)
+  })
 })
