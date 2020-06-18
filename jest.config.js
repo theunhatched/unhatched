7bdd74806c7ffd83b567ad64d1ca872d2b3bd9e5
Philihp Busby <philihp@gmail.com>
2020-06-18 23:52:47 +0000

    update eslint for jest and unicorn (#40)
    * remove handler.js from serverless failed setup
    
    * update eslint-config to 2.0.0:
    
    * repair tests
    
    * update eslint-config to 2.0.1
    
    * ignore .next folder
    
    * jest must ignore .next dir

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', 'setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/mocks.js',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/mocks.js',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 }
