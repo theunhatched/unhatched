8f2681783ef94c30a8e6c4156ec4ebb4417d4d70
Philihp Busby <philihp@gmail.com>
2020-06-25 02:49:12 +0000

    add component tests for coverage

---
+++ components/__tests__/footer.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Footer from '../footer'
+
+describe('footer', () => {
+  it('renders a footer', () => {
+    expect.assertions(1)
+    const wrapper = mount(<Footer />)
+    expect(wrapper.find('footer').text()).toBe('Built with ♥ in San Francisco')
+  })
+})
