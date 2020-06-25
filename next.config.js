8065785667a926de88b0534f4e5a947cc61ad61a
Philihp Busby <philihp@gmail.com>
2020-06-25 03:42:05 +0000

    exclude tests from next.js building

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   target: 'serverless',
+  excludeFile: (path) => /\*.{spec,test}.js/.test(path),
   webpack: (config) => ({
     ...config,
     // Fixes npm packages that depend on `fs` module
