b5e3f415a1a166c402f71f80bc6ed8ee12c01928
Philihp Busby <philihp@gmail.com>
2020-07-09 21:10:36 +0000

    remove unnessary library

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-require('dotenv').config()
-const path = require('path')
-const Dotenv = require('dotenv-webpack')
-
 module.exports = {
   webpack: (config, { webpack }) => ({
     ...config,
-    plugins: [
-      ...config.plugins,
-      new webpack.IgnorePlugin(/__tests__\/.*/),
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ],
+    plugins: [...config.plugins, new webpack.IgnorePlugin(/__tests__\/.*/)],
   }),
 }
