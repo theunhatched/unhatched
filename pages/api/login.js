0657415bb3bb107d847ddc9c64a8256095ff925f
Philihp Busby <philihp@gmail.com>
2020-07-12 01:38:45 +0000

    tell me something i dont already know

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,10 @@
 import auth0 from '../../lib/auth0'
+import config from '../../lib/config'
 
 export default async function login(request, response) {
   try {
+    console.log('LOGIN')
+    console.log(config)
     await auth0.handleLogin(request, response)
   } catch (error) {
     console.error(error)
