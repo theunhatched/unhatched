812f1cd3f3dab64fbea3bff90389409ffe3459c3
Philihp Busby <philihp@gmail.com>
2020-07-16 23:41:38 +0000

    uses environment variables unless not provided, then figures out path from request

--- pages/api/me.js
+++ pages/api/me.js
@@ -2,7 +2,7 @@ import auth0 from '../../lib/auth0'
 
 export default async function me(request, response) {
   try {
-    await auth0.handleProfile(request, response)
+    await auth0(request).handleProfile(request, response)
   } catch (error) {
     console.error(error)
     response.status(error.status || 500).end(error.message)
