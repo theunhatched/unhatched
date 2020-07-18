e33dcd449b5e9bae4c3e2cc91fdb2857659f6aa6
Philihp Busby <philihp@gmail.com>
2020-07-18 03:16:12 +0000

    bug: debug all variables from server (#77)
    * debug all variables from server
    
    * include request url
    
    * explicit properties
    
    * show url
    
    * show both urls
    
    * redirect if not the same url
    
    * better way to do a redirect
    
    * redirect to correct url
    
    * fix redirects for preview domain

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,11 +1,19 @@
+import absoluteUrl from 'next-absolute-url'
 import auth0 from '../../lib/auth0'
-import config from '../../lib/config'
 
 export default async function login(request, response) {
   try {
-    console.log('LOGIN')
-    console.log(config)
-    await auth0.handleLogin(request, response)
+    // TODO: this should happen at every URL to redirect to canonical URLs.
+    const baseUrl = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : 'http://localhost:3000'
+    const requestUrl = absoluteUrl(request).origin
+    if (baseUrl !== requestUrl) {
+      response.writeHead(301, { Location: `${baseUrl}${request.url}` })
+      response.end()
+    } else {
+      await auth0.handleLogin(request, response)
+    }
   } catch (error) {
     console.error(error)
     response.status(error.status || 500).end(error.message)
