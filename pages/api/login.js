4c4c0320e63832b5373335fa6520c6730cfbe191
Philihp Busby <philihp@gmail.com>
2020-07-18 03:00:15 +0000

    fix redirects for preview domain

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,25 +1,19 @@
 import absoluteUrl from 'next-absolute-url'
 import auth0 from '../../lib/auth0'
-import config from '../../lib/config'
 
 export default async function login(request, response) {
   try {
+    // TODO: this should happen at every URL to redirect to canonical URLs.
     const baseUrl = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : 'http://localhost:3000'
     const requestUrl = absoluteUrl(request).origin
-    console.log('=============')
-    console.log({ baseUrl })
-    console.log({ requestUrl })
-    console.log(request.url)
-    console.log('protocol', request.protocol)
-    console.log('encryptd', request.connection.encrypted)
-    console.log('=======')
     if (baseUrl !== requestUrl) {
       response.writeHead(301, { Location: `${baseUrl}${request.url}` })
-      return response.end()
+      response.end()
+    } else {
+      await auth0.handleLogin(request, response)
     }
-    await auth0.handleLogin(request, response)
   } catch (error) {
     console.error(error)
     response.status(error.status || 500).end(error.message)
