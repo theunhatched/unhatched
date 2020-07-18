76e119c3c5fb83b40afdc26ab1be5d792ffa7930
Philihp Busby <philihp@gmail.com>
2020-07-18 01:03:40 +0000

    show url

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,17 +1,11 @@
 import auth0 from '../../lib/auth0'
 import config from '../../lib/config'
+import absoluteUrl from 'next-absolute-url'
 
 export default async function login(request, response) {
   try {
     console.log('LOGIN')
-    console.log(config)
-    console.log('REQ')
-    console.log(request.host)
-    console.log(request.protocol)
-    console.log(request.method)
-    console.log(request.path)
-    console.log('ENV')
-    console.log(process.env)
+    console.log(absoluteUrl(request).origin)
     console.log('END')
     await auth0.handleLogin(request, response)
   } catch (error) {
