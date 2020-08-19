a29c5d5d3bb6a334fe725e0645713f1fc66b67a0
Philihp Busby <philihp@gmail.com>
2020-08-19 01:44:11 +0000

    database api sanity checker

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,16 +1,37 @@
 import { gql, ApolloServer } from 'apollo-server-micro'
+import knex from 'knex'
+
+const db = knex({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+})
 
 const typeDefs = gql`
   type Query {
-    hello: String!
+    bepis(first: Int = 25, skip: Int = 0): [Bepis!]!
+  }
+
+  type Bepis {
+    id: ID!
+    name: String!
+    description: String!
   }
 `
 
 const resolvers = {
   Query: {
-    hello: (_parent, _args, _context) => {
-      return 'helo'
+    bepis: (_parent, args, _context) => {
+      return db
+        .select('*')
+        .from('bepis')
+        .orderBy('name', 'asc')
+        .limit(Math.min(args.first, 10))
+        .offset(args.skip)
     },
+  },
+
+  Bepis: {
+    id: (bepis, _args, _context) => bepis.id,
   },
 }
 
