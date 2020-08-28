83a56cd69b7816d7aa5854ac48b754693a5e12e8
Philihp Busby <philihp@gmail.com>
2020-08-28 11:10:20 +0000

    add a test for mocked DB

--- pages/api/__tests__/donors.test.js
+++ pages/api/__tests__/donors.test.js
@@ -3,11 +3,14 @@ import fetch from 'isomorphic-unfetch'
 import listen from 'test-listen'
 import { apiResolver } from 'next/dist/next-server/server/api-utils'
 import handler from '../donors'
+import db from '../../../lib/db'
 
 describe('donors', () => {
   let server
   let url
   beforeAll(async (done) => {
+    await db.migrate.latest()
+    await db.seed.run()
     server = http.createServer((req, res) =>
       apiResolver(req, res, undefined, handler)
     )
@@ -20,8 +23,16 @@ describe('donors', () => {
 
   test('200', async () => {
     const response = await fetch(url)
-    // const jsonResult = await response.json()
+    const jsonResult = await response.json()
     expect(response.status).toBe(200)
-    // expect(jsonResult).toBe({})
+    expect(jsonResult[0]).toEqual(
+      expect.objectContaining({ id: 1, name: 'alice' })
+    )
+    expect(jsonResult[1]).toEqual(
+      expect.objectContaining({ id: 2, name: 'betty' })
+    )
+    expect(jsonResult[2]).toEqual(
+      expect.objectContaining({ id: 3, name: 'cindy' })
+    )
   })
 })
