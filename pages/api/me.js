171c872765d390a3b99eb0d6c54730fabc4d724f
Philihp Busby <philihp@gmail.com>
2020-09-08 06:40:01 +0000

    cleanup

--- pages/api/me.js
+++ pages/api/me.js
@@ -2,10 +2,9 @@ import auth0 from '../../lib/auth0'
 
 export default async function me(req, res) {
   try {
-    console.log('/api/me')
     await auth0.handleProfile(req, res)
   } catch (error) {
-    console.error('/api/me', error)
+    console.error(error)
     res.status(error.status || 500).end(error.message)
   }
 }
