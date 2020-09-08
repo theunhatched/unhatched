e666a5b949a7a05513abb85dc4408c2a903f4a1d
Philihp Busby <philihp@gmail.com>
2020-09-08 22:05:26 +0000

    test api route

--- pages/api/__tests__/me.test.js
+++ pages/api/__tests__/me.test.js
@@ -3,29 +3,10 @@ import * as auth0 from '../../../lib/auth0'
 
 describe('auth0', () => {
   it('runs a test', () => {
+    expect.assertions(1)
     const handleProfile = jest.fn()
     auth0.default = { handleProfile }
     me('REQ', 'RES')
-    // expect(auth0.default).toBeCalledWith({})
-
-    // expect.assertions(1)
-    // const request = {}
-    // const response = {
-    //   statusCode: 0,
-    //   json: jest.fn(),
-    // }
-    // hello(request, response)
-    // expect(response.statusCode).toBe(200)
+    expect(handleProfile).toHaveBeenCalled()
   })
 })
-
-// import auth0 from '../../lib/auth0'
-
-// export default async function me(req, res) {
-//   try {
-//     await auth0.handleProfile(req, res)
-//   } catch (error) {
-//     console.error(error)
-//     res.status(error.status || 500).end(error.message)
-//   }
-// }
