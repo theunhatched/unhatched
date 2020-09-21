7119c9dedd68933734b8c95666d7709f04b2cd94
Philihp Busby <philihp@gmail.com>
2020-09-21 09:39:48 +0000

    dashboard page lists data from hasura

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -2,6 +2,7 @@ import auth0 from '../../lib/auth0'
 
 export default async function callback(req, res) {
   try {
+    console.log({ query: req.query })
     await auth0.handleCallback(req, res, { redirectTo: '/' })
   } catch (error) {
     console.error(error)
