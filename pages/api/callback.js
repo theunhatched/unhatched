0692f8068d88f0e7c07fd95262b633577618aa2c
Philihp Busby <philihp@gmail.com>
2020-09-21 10:21:56 +0000

    remove debug:

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -2,7 +2,6 @@ import auth0 from '../../lib/auth0'
 
 export default async function callback(req, res) {
   try {
-    console.log({ query: req.query })
     await auth0.handleCallback(req, res, { redirectTo: '/' })
   } catch (error) {
     console.error(error)
