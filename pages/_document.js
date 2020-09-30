bfc99caf2158ca26317f0a31e7f7b337ded84481
Philihp Busby <philihp@gmail.com>
2020-09-30 01:57:43 +0000

    add google analytics

---
+++ pages/_document.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { useAmp } from 'next/amp'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
+import AmpAnalytics from '../components/amp/amp-analytics'
+
+const AmpWrap = ({ ampOnly, nonAmp }) => {
+  const isAmp = useAmp()
+  if (ampOnly) return isAmp && ampOnly
+  return !isAmp && nonAmp
+}
+
+class _Document extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+
+          {/* AMP - Google Analytics */}
+          <AmpWrap
+            ampOnly={
+              <AmpAnalytics
+                type="googleanalytics"
+                script={{
+                  vars: {
+                    account: GA_TRACKING_ID,
+                    gtag_id: GA_TRACKING_ID,
+                    config: {
+                      [GA_TRACKING_ID]: { groups: 'default' },
+                    },
+                  },
+                  triggers: {
+                    trackPageview: {
+                      on: 'visible',
+                      request: 'pageview',
+                    },
+                  },
+                }}
+              />
+            }
+          />
+
+          {/* Non-AMP - Google Analytics */}
+          <AmpWrap
+            nonAmp={
+              <>
+                <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                />
+                <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}');
+                  `,
+                  }}
+                />
+              </>
+            }
+          />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default _Document
