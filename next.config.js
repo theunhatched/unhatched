c01b3d53d52a392ca00ace9939ec99c8600be014
Philihp Busby <philihp@gmail.com>
2020-10-01 12:03:04 +0000

    wip

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 module.exports = {
   webpack: (config, { webpack }) => ({
@@ -10,7 +12,7 @@ module.exports = {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_SCOPE: 'openid profile offline_access',
+    AUTH0_SCOPE: 'openid profile',
     REDIRECT_URI:
       process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
     POST_LOGOUT_REDIRECT_URI:
@@ -18,5 +20,6 @@ module.exports = {
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
     HASURA_GRAPHQL_URL: process.env.HASURA_GRAPHQL_URL,
     SESSION_COOKIE_LIFETIME: 7200, // 2 hours
+    APP_HOST: process.env.DOMAIN,
   },
 }
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,13 @@
 import React from 'react'
 
-import HeroLayout from '../components/hero-layout'
-import Button from '../components/button'
+import { useFetchUser } from '../lib/user'
+import { withApollo } from '../lib/withApollo'
 
-export default function Home() {
-  return (
-    <HeroLayout>
-      <div className="Index">
-        {/* <img src="/woman-illustration.svg" className="illustration" alt="" /> */}
-        <h2 className="title">We&apos;re a woman-first fertility service.</h2>
-        <p className="subtitle">
-          We’re run by by a team of WoMen for women. Our mission is to educate,
-          prioritize, and match Egg, Embryo, and Surrogacy donors and potential
-          families in a totally transparent, step-by-step process.
-        </p>
-        <Button href="/become-a-donor" primary>
-          Got Eggs?
-        </Button>
-        <Button href="/about-us">Learn More</Button>
-        <style jsx>{`
-          .Index {
-            padding: 50px 20px;
-          }
-          .title {
-            /* We’re a woman-first fertility service. */
-            font-family: Playfair Display;
-            font-style: normal;
-            font-weight: 900;
-            font-size: 76px;
-          }
-          .subtitle {
-            width: 520px;
-            font-family: Ruda;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 20px;
-            line-height: 180%;
-            /* or 36px */
-            padding: 20px 0;
-          }
-        `}</style>
-      </div>
-    </HeroLayout>
-  )
+const Index = () => {
+  const { user, loading } = useFetchUser()
+  if (loading) return <>loading</>
+  if (!loading && !user) return <>logged out</>
+  return <div>Hello</div>
 }
+
+export default Index
