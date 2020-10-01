bf983104b8d2706174206f54f2d648e4f5a5b2f7
Philihp Busby <philihp@gmail.com>
2020-10-01 13:46:56 +0000

    repair tests

--- pages/api/__tests__/login.test.js
+++ pages/api/__tests__/login.test.js
@@ -1,13 +1,11 @@
 import login from '../login'
 import * as auth0 from '../../../lib/auth0'
 
-describe('auth0', () => {
+describe('api/login', () => {
   it('runs a test', () => {
     expect.assertions(1)
     const handleLogin = jest.fn()
-    auth0.default = () => ({
-      handleLogin,
-    })
+    auth0.default = { handleLogin }
     login()
     expect(handleLogin).toHaveBeenCalled()
   })
