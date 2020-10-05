189e8f30f75e7442721dc63f82be4e8ebaad5baa
Philihp Busby <philihp@gmail.com>
2020-10-05 13:00:36 +0000

    test api/session

--- pages/api/session.js
+++ pages/api/session.js
@@ -7,9 +7,9 @@ export default async function session(req, res) {
     res.status(200).json({ accessToken })
   } catch (error) {
     console.error(error)
-    res.status(error.status || 500).json({
-      code: error.code,
-      error: error.message,
-    })
+    // res.status(error.status || 500).json({
+    //   code: error.code,
+    //   error: error.message,
+    // })
   }
 }
