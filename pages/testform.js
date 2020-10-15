f2ffd180bd9506297c05f86e9915e4c1308c6971
Philihp Busby <philihp@gmail.com>
2020-10-15 05:22:47 +0000

    form with table

--- pages/testform.js
+++ pages/testform.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag'
-import { Frontier } from 'frontier-forms'
-import { ApolloConsumer } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/react-hooks'
 import TestNav from '../components/test-nav'
 import { withApollo } from '../lib/withApollo'
 import { useFetchUser } from '../lib/user'
 
 const mutation = gql`
   mutation(
-    $user_id: String!
+    $userId: uuid!
     $profile: String!
-    $hair_color: String!
-    $eye_color: String!
-    $birthdate: String!
+    $hairColor: String!
+    $eyeColor: String!
+    $birthdate: date!
   ) {
     insert_donors_one(
       object: {
-        user_id: $user_id
+        user_id: $userId
         profile: $profile
-        eye_color: $eye_color
-        hair_color: $hair_color
+        eye_color: $eyeColor
+        hair_color: $hairColor
         birthdate: $birthdate
       }
     ) {
@@ -28,90 +27,131 @@ const mutation = gql`
   }
 `
 
+const query = gql`
+  query {
+    donors {
+      id
+      hair_color
+      eye_color
+      created_at
+      birthdate
+      user_id
+      updated_at
+      profile
+    }
+  }
+`
+
 const TestForm = () => {
-  const { user, loading: userLoading, error } = useFetchUser()
+  const { user, loading: userLoading, error: userError } = useFetchUser()
+
+  const [userIdInput, setUserId] = useState(
+    '9bf03504-bedb-4fe9-832b-cce5c693458e'
+  )
+  const [profileInput, setProfile] = useState('## My Profile')
+  const [hairColorInput, setHairColor] = useState('Blonde')
+  const [eyeColorInput, setEyeColor] = useState('Blue')
+  const [birthdateInput, setBirthdate] = useState('1984-02-29')
+
+  const [addDonor] = useMutation(mutation)
+  const {
+    loading: donorsLoading,
+    error: donorsError,
+    data: donorData,
+  } = useQuery(query)
 
   if (userLoading) {
     return <div>user loading</div>
   }
-  if (error) {
-    return <div>{error}</div>
+  if (donorsLoading) {
+    return <div>donors loading</div>
+  }
+  if (userError) {
+    return <div>{userError}</div>
+  }
+  if (donorsError) {
+    return <pre>{donorsError}</pre>
   }
 
   return (
-    <ApolloConsumer>
-      {(client) => (
-        <>
-          <TestNav />
-          User: {JSON.stringify(user)}
-          <h1>Form</h1>
-          <Frontier client={client} mutation={mutation}>
-            {({ state, modifiers }) => {
-              return (
-                <form onSubmit={() => modifiers.save()}>
-                  <div>
-                    <input
-                      type="text"
-                      name="user_id"
-                      value={state.values.user_id}
-                      onChange={modifiers.user_id.change}
-                    />
-                    {state.errors.user_id && (
-                      <p>Error: {state.errors.user_id}</p>
-                    )}
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      name="profile"
-                      value={state.values.profile}
-                      onChange={modifiers.profile.change}
-                    />
-                    {state.errors.profile && (
-                      <p>Error: {state.errors.profile}</p>
-                    )}
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      name="hair_color"
-                      value={state.values.hair_color}
-                      onChange={modifiers.hair_color.change}
-                    />
-                    {state.errors.hair_color && (
-                      <p>Error: {state.errors.hair_color}</p>
-                    )}
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      name="eye_color"
-                      value={state.values.eye_color}
-                      onChange={modifiers.eye_color.change}
-                    />
-                    {state.errors.eye_color && (
-                      <p>Error: {state.errors.eye_color}</p>
-                    )}
-                  </div>
-                  <div>
-                    <input
-                      type="text"
-                      name="birthdate"
-                      value={state.values.birthdate}
-                      onChange={modifiers.birthdate.change}
-                    />
-                    {state.errors.birthdate && (
-                      <p>Error: {state.errors.birthdate}</p>
-                    )}
-                  </div>
-                  <input type="submit" value="Save" />
-                </form>
-              )
-            }}
-          </Frontier>
-        </>
-      )}
-    </ApolloConsumer>
+    <>
+      <TestNav />
+      User: {JSON.stringify(user)}
+      <h1>Form</h1>
+      <form
+        onSubmit={(e) => {
+          console.log('submit')
+          e.preventDefault()
+          addDonor({
+            variables: {
+              userId: userIdInput,
+              profile: profileInput,
+              hairColor: hairColorInput,
+              eyeColor: eyeColorInput,
+              birthdate: birthdateInput,
+            },
+          })
+          console.log('done')
+        }}
+      >
+        <div>
+          <input
+            type="text"
+            name="userId"
+            value={userIdInput}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="profile"
+            value={profileInput}
+            onChange={(e) => setProfile(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="hairColor"
+            value={hairColorInput}
+            onChange={(e) => setHairColor(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="eyeColor"
+            value={eyeColorInput}
+            onChange={(e) => setEyeColor(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="birthdate"
+            value={birthdateInput}
+            onChange={(e) => setBirthdate(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Save" />
+      </form>
+      <h3>Donors</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <pre>{JSON.stringify(donorData, undefined, 2)}</pre>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
