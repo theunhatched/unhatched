b340dd889e012f3c76d2a4da0abb9c47ae915d19
Philihp Busby <philihp@gmail.com>
2020-10-29 02:39:04 +0000

    update to enzyme-adapter 17, when it releases

--- setup-tests.js
+++ setup-tests.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import EnzymeAdapter from 'enzyme-adapter-react-17'
 import enableHooks from 'jest-react-hooks-shallow'
 import 'bdd-lazy-var/global'
 
